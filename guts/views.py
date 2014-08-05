d1fa2847861a0cf1bc8aefdcfc5c1036d49ef2c2
Sam VB <sjohnson540@gmail.com>
2014-08-05 19:43:58 -0400

    added user model and made basic file structure

--- guts/views.py
+++ guts/views.py
@@ -1,5 +1,9 @@
 from guts import app
+from guts import db
+from guts.models import User
 
 @app.route('/')
 def index():
-    return "Hello Julian"
+    users = User.query.all()
+    print users
+    return "hello " + users[0].username
--- runserver.py
+++ runserver.py
@@ -1,2 +1,10 @@
 from guts import app
+from guts import db
+from guts.models import User
+'''
+db.create_all()
+sam = User('sam', 'sam@sam.com')
+db.session.add(sam)
+db.session.commit()'''
+
 app.run(debug=True)
