34b152fe224923844ba97b26b3b50d3b9a6a64c5
Sam VB <sjohnson540@gmail.com>
2014-08-05 21:37:55 -0400

    added templating example

--- guts/views.py
+++ guts/views.py
@@ -1,9 +1,17 @@
 from guts import app
 from guts import db
 from guts.models import User
+from flask import session, redirect, url_for, render_template, flash
 
-@app.route('/')
-def index():
+@app.route('/users/')
+def list_users():
     users = User.query.all()
-    print users
-    return "hello " + users[0].username
+    return render_template("list_users.html", users=users)
+
+@app.route('/users/add', methods=["POST"])
+def add_user():
+    user = User(request.form['username'], request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    flash("New User Added!")
+    return redirect(url_for('list_users'))
